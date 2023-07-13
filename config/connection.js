10fa986e9afd7f0a7d5b1562b6756b8618d30633
Beerendan <brendanrbowen@gmail.com>
2023-07-13 13:29:16 -0400

    testing for heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
-const Sequelize= require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
-let sequelize;
 
-if(process.env.JAWSDB_URL){
-    sequelize= new Sequelize(process.env.JAWSDB_URL);
-}   else{
-    sequelize= new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host:'localhost',
-            dialect:'mysql',
-            port: 3306,
-        }
-    );
-}
-module.exports= sequelize;
+module.exports = {
+    HOST: process.env.HOST,
+    USER: 'root',
+    PASSWORD: process.env.PASSWORD,
+    DB: "recipe_db",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
