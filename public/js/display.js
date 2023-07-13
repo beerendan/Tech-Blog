60317a82542dac3bd7627624618728f53f5f0332
Beerendan <brendanrbowen@gmail.com>
2023-07-13 12:37:35 -0400

    tweaking and debugging completed

--- public/js/display.js
+++ public/js/display.js
@@ -25,3 +25,49 @@ async function displayPosts() {
     console.log(item);
     console.log(item[0].title);
 };
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#title').value.trim();
+    const body = document.querySelector('#body').value.trim();
+  
+    if (title && body) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, body }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+  document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
